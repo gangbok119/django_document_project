eb24ab4347c7af214146da1827819d71d699f928
Kangbok Lee <gangbok119@gmail.com>
2017-10-10 14:06:48 +0900

    다대일 관계, 재귀적 다대일 관계

--- django_document/model/models.py
+++ django_document/model/models.py
@@ -30,3 +30,14 @@ class Car(models.Model):
 
     def __str__(self):
         return ('브랜드명 : {}, 차종 : {}'.format(self.manufacturer.name,self.name))
+
+
+
+
+class User(models.Model):
+    name= models.CharField(max_length=30)
+
+    teacher = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return ('이름 : {}'.format(self.name))
