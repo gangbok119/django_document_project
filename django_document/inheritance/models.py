9d076e07794cf7164c86235f6fe5b1efac13246b
Kangbok Lee <gangbok119@gmail.com>
2017-10-11 16:09:09 +0900

    추상 기본 클래스 사용법

--- django_document/inheritance/models.py
+++ django_document/inheritance/models.py
@@ -1,21 +1,35 @@
 from django.db import models
 
-__all__=(
+__all__ = (
 
-    'CommonInfo','Student','Teacher'
+    'CommonInfo', 'Student', 'Teacher'
 )
+
 
 # Create your models here.
 
+class School(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# 역참조 매니저를 상속시키는 방법까지
 class CommonInfo(models.Model):
+    school = models.ForeignKey(School, blank=True, null=True,
+                               related_name="%(app_label)s_%(class)s_related",
+                               related_query_name="%(app_label)s_%(class)ss",
+                               )
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
 
     class Meta:
         abstract = True
 
+
 class Student(CommonInfo):
     home_group = models.CharField(max_length=5)
+
 
 class Teacher(CommonInfo):
     subject = models.CharField(max_length=30)
