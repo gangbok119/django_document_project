b8b01a400b2b2307b4ceba568ca394ad2aa873ef
Kangbok Lee <gangbok119@gmail.com>
2017-10-11 11:58:31 +0900

    중계 모델이 외래키를 여럿 가질 때 역참조 문제 해결

--- django_document/model/models/many_to_many/Member_Group.py
+++ django_document/model/models/many_to_many/Member_Group.py
@@ -12,15 +12,17 @@ class Idol(models.Model):
 class Group(models.Model):
     name = models.CharField(max_length=30)
     debut_date = models.DateField()
-    members = models.ManyToManyField(Idol, through='Membership')
+    members = models.ManyToManyField(Idol, through='Membership',through_fields=('group','idol'))
 
     def __str__(self):
         return self.name
 
 
 class Membership(models.Model):
-    idol = models.ForeignKey(Idol, on_delete=models.CASCADE)
+    idol = models.ForeignKey(Idol, on_delete=models.CASCADE, related_name='membership_set')
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    #recommender = models.ForeignKey(Idol, on_delete=models.SET_NULL,null=True, related_name='recommend_membership_set' )
+    recommenders = models.ManyToManyField(Idol, blank=True, related_name='recommenders_membership_set', )
     date_joined = models.DateField()
     is_active = models.BooleanField()
 
