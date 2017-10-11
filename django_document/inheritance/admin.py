68a21dd65984ddc01fe38247c8419c97589cb72c
Kangbok Lee <gangbok119@gmail.com>
2017-10-11 18:02:41 +0900

    proxy 모델

--- django_document/inheritance/admin.py
+++ django_document/inheritance/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Student, Teacher
+from .models import Student, Teacher, Place, Restaurant, Champion
+
+class ChampionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'champion_type', 'rank',)
+    list_editable = ('rank')
+    ordering = ('rank')
 
 admin.site.register(Student)
 admin.site.register(Teacher)
+admin.site.register(Place)
+admin.site.register(Restaurant)
+admin.site.register(Champion, ChampionAdmin)
---
+++ django_document/inheritance/models/proxy.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+__all__ = (
+
+    'Champion',
+)
+
+class Champion(models.Model):
+    CHOICE_TYPE = (
+        ('magician','마법사'),
+        ('supporter','서포터'),
+        ('ad','원거리 딜러')
+    )
+    champion_type = models.CharField(max_length=20, choices=CHOICE_TYPE)
+    name = models.CharField(max_length=30)
+    rank = models.PositiveIntegerField(default=0)
+
+
+    def __str__(self):
+        return ('{} ( {})'.format(self.name,self.get_champion_type_display()))
+
